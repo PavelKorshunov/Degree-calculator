e69adb88b9f78b8386c6ae984b0bf4ef5f4b73f4
Павел <korsun4@mail.ru>
2019-01-14 23:08:38 +0300

    add Celsius

--- degree.c
+++ degree.c
@@ -1,12 +1,24 @@
-#include "stdio.h"
+#include <stdio.h>
 
 int main()
 {
-	int degree, result;
+	// Перевод из градусов цельсия в фаренгейт
+	int degreec, resultc;
 
 	printf("Enter the degree ");
 
-	scanf("%d", &degree);
-	result = (degree * 1.8) + 32;
-	printf("Degrees Fahrenheit %d\n", result);
+	scanf("%d", &degreec);
+
+	resultc = (degreec * 1.8) + 32;
+	printf("Degrees Fahrenheit %d\n", resultc);
+
+	// Перевод из фаренгейта в цельсия
+	float degreef, resultf;
+
+	printf("Enter the degree F ");
+
+	scanf("%f", &degreef);
+	resultf = (degreef - 32) / 1.8;
+
+	printf("Degrees Celsius %f\n", resultf);
 }
